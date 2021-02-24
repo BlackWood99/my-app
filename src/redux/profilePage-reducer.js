1bc8e11437538c643fc1f18e97c3883972567899
BlackWood99 <78879606+BlackWood99@users.noreply.github.com>
2021-02-24 14:21:44 +0300

    Update profilePage-reducer.js

--- src/redux/profilePage-reducer.js
+++ src/redux/profilePage-reducer.js
@@ -8,8 +8,8 @@ const UPDATE_STATUS = 'UPDATE_STATUS'
 
 let initialState = {
     posts: [
-        {id: 1, text: 'hihi, bitches'},
-        {id: 2, text: 'do you like me?)'},
+        {id: 1, text: 'hihi, my followers'},
+        {id: 2, text: 'do you like music?)'},
     ],
     postText: '',
     profile: null,
