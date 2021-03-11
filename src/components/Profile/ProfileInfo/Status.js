31c793105db66b75dab3519e04b56e4fe86a75eb
BlackWood99 <78879606+BlackWood99@users.noreply.github.com>
2021-03-11 11:16:06 +0300

    useEffect for status
    added useEffect instead class methods

--- src/components/Profile/ProfileInfo/Status.js
+++ src/components/Profile/ProfileInfo/Status.js
@@ -1,56 +1,95 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from './ProfilePerson.module.css'
 
 
-class Status extends React.Component {
 
-    state = {
-        editMode: false,
-        status: this.props.status || 'NO STATUS'
+const Status = (props) => {
+
+    const [editMode, setEditMode]  = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
+
+    const activateEditMode = () => {
+        setEditMode(true)
     }
 
-    activateEditMode = () => {
-        this.setState({
-            editMode: true
-        })
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
     }
 
-    deactivateEditMode = () => {
-        this.setState({
-            editMode: false
-        })
-        this.props.updateStatus(this.state.status)
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
     }
 
-    onStatusChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        
-        if (prevProps.status !== this.props.status) {
-            this.setState({
-                status: this.props.status
-            })
-        }
-    }
-
-    render() {  
-        return (
-            <div className={styles.status}>
+    return (
+        <div className={styles.status}>
                 {
-                    !this.state.editMode && 
-                        <span onDoubleClick={() => this.activateEditMode()}>{this.state.status}</span>
+                    !editMode && 
+                        <span onDoubleClick={() => activateEditMode()}>{status}</span>
                 }
                 {
-                    this.state.editMode && 
-                        <input autoFocus={true} onChange={this.onStatusChange} onBlur={() => this.deactivateEditMode()} value={this.state.status}/>
+                    editMode && 
+                        <input autoFocus={true} onChange={onStatusChange} 
+                            onBlur={() => deactivateEditMode()} value={status}/>
                 }
             </div>
-        )
-    }
+    )
 }
+
+
+// class Status extends React.Component {
+
+//     state = {
+//         editMode: false,
+//         status: this.props.status || 'NO STATUS'
+//     }
+
+//     activateEditMode = () => {
+//         this.setState({
+//             editMode: true
+//         })
+//     }
+
+//     deactivateEditMode = () => {
+//         this.setState({
+//             editMode: false
+//         })
+//         this.props.updateStatus(this.state.status)
+//     }
+
+//     onStatusChange = (e) => {
+//         this.setState({
+//             status: e.currentTarget.value
+//         })
+//     }
+
+//     componentDidUpdate(prevProps, prevState) {
+        
+//         if (prevProps.status !== this.props.status) {
+//             this.setState({
+//                 status: this.props.status
+//             })
+//         }
+//     }
+
+//     render() {  
+//         return (
+//             <div className={styles.status}>
+//                 {
+//                     !this.state.editMode && 
+//                         <span onDoubleClick={() => this.activateEditMode()}>{this.state.status}</span>
+//                 }
+//                 {
+//                     this.state.editMode && 
+//                         <input autoFocus={true} onChange={this.onStatusChange} onBlur={() => this.deactivateEditMode()} value={this.state.status}/>
+//                 }
+//             </div>
+//         )
+//     }
+// }
 
 export default Status
